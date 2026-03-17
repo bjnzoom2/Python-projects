fba970172ef781fdedc41683ad03e36039cd9829
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-17 12:37:31 +0800

    Calculator

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
-x = 2
-for i in range(10) :
-    print(x)
-    x *= 2
+operator = input("Enter an operator (+, -, *, /): ")
+num1 = int(input("Enter first num: "))
+num2 = int(input("Enter second num: "))
+
+if (operator == "+") :
+    print(f"{num1} + {num2} = {num1 + num2}")
+elif (operator == "-") :
+    print(f"{num1} - {num2} = {num1 - num2}")
+elif (operator == "*") :
+    print(f"{num1} * {num2} = {num1 * num2}")
+elif (operator == "/") :
+    print(f"{num1} / {num2} = {num1 / num2}")
