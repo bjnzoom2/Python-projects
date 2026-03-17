eca68b88aa7df65832556d3a7621c6762e5ba976
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-17 14:14:15 +0800

    Condition

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
-unit1 = input("Enter unit1 (°C, °F): ")
-tempValue = float(input("Enter temperature: "))
-unit2 = input("Enter unit2 (°C, °F): ")
-if (unit1 == "°C" and unit2 == "°F") :
-    print(f"{tempValue}°C = {tempValue * 9.0/5.0 + 32}°F")
-elif (unit1 == "°F" and unit2 == "°C") :
-    print(f"{tempValue}°F = {(tempValue - 32) * 5.0/9.0}°C")
-else:
-    print("Invalid conversion")
+x = int(input("Enter num: "))
+print(True if x % 2 == 0 else False)
