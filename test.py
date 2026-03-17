57558ec8df482fa6d17f07023e5f988a7cbc3cbf
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-17 15:10:08 +0800

    Indexing

--- test.py
+++ test.py
@@ -1,9 +1,2 @@
-name = input("Enter name: ")
-name = name.capitalize()
-result = len(name)
-
-for i in range(len(name)):
-    if (name[i].isspace):
-        result -= 1
-
-print(name, result)
+string = "unhifwueh"
+print(string[0:8:2])
