067d90c175676b05a7224d12aec8b72b0036e9f7
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-17 21:07:01 +0800

    Countdown

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-initAmount = float(input("Enter principal amount: $"))
-annualInterest = float(input("Enter annual interest rate: ")) / 100
-compoundingFrequency = float(input("Enter compounding frequency: "))
-time = float(input("Enter amount of time: "))
-finalAmount = 0
+import time
 
-finalAmount = initAmount * (1 + annualInterest / compoundingFrequency) ** (compoundingFrequency * time)
-print(f"Principal Amount: ${initAmount:.2f}\nAnnual Interest: {annualInterest * 100:.2f}%\nCompounding Frequency: {compoundingFrequency:.2f}\nTime: {time:.2f} years\nFinal Amount: ${finalAmount:.2f}")
+timer = int(input("Enter countdown: "))
+for i in range(timer):
+    print(timer - i)
+    time.sleep(1)
+print("Times up")
