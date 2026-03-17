7150ded87cecc02fd94b2f3df937574ed1c717ce
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-17 11:25:36 +0800

    Python testing

---
+++ test.py
@@ -0,0 +1,6 @@
+story = "The boy ate ____ and died from ____ "
+print(story)
+firstBlank =  input("Enter first blank: ")
+secondBlank =  input("Enter second blank: ")
+story = "The boy ate " + firstBlank + " and died from " + secondBlank
+print(story)
