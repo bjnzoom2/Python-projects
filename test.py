5787621df6dd1583cde82abc8894bd2c19420628
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-17 13:32:42 +0800

    Temperature unit converter

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
-choice = input("(kg to lbs | lbs to kg): ")
-if (choice == "kg to lbs") :
-    kg = float(input("Enter kg: "))
-    print(f"{kg} kg = {kg * 2.205} lbs")
-elif (choice == "lbs to kg"):
-    lbs = float(input("Enter lbs: "))
-    print(f"{lbs} lbs = {lbs / 2.205} kg")
+unit1 = input("Enter unit1 (°C, °F): ")
+tempValue = float(input("Enter temperature: "))
+unit2 = input("Enter unit2 (°C, °F): ")
+if (unit1 == "°C" and unit2 == "°F") :
+    print(f"{tempValue}°C = {tempValue * 9.0/5.0 + 32}°F")
+elif (unit1 == "°F" and unit2 == "°C") :
+    print(f"{tempValue}°F = {(tempValue - 32) * 5.0/9.0}°C")
 else:
-    print("Invalid choice")
+    print("Invalid conversion")
