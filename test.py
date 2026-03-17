c5b45d4bc67243f33be2f303e125df397ed04e14
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-17 12:43:09 +0800

    Floats

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 operator = input("Enter an operator (+, -, *, /): ")
-num1 = int(input("Enter first num: "))
-num2 = int(input("Enter second num: "))
+num1 = float(input("Enter first num: "))
+num2 = float(input("Enter second num: "))
 
 if (operator == "+") :
     print(f"{num1} + {num2} = {num1 + num2}")
