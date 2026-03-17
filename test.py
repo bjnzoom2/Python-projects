a41d2b31223ebe0391e57c9f2c41a7c72c24d05b
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-17 20:51:21 +0800

    Compound Interest Calculator

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-price1 = 3.1415926
-price2 = 192874.1
+initAmount = float(input("Enter principal amount: $"))
+annualInterest = float(input("Enter annual interest rate: ")) / 100
+compoundingFrequency = float(input("Enter compounding frequency: "))
+time = float(input("Enter amount of time: "))
+finalAmount = 0
 
-print(f"{price1:20}")
-print(f"{price2:3f}")
+finalAmount = initAmount * (1 + annualInterest / compoundingFrequency) ** (compoundingFrequency * time)
+print(f"Principal Amount: ${initAmount:.2f}\nAnnual Interest: {annualInterest * 100:.2f}%\nCompounding Frequency: {compoundingFrequency:.2f}\nTime: {time:.2f} years\nFinal Amount: ${finalAmount:.2f}")
