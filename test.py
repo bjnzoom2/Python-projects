1d81871130a8172b0cf4423581186c2cad3f7498
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-18 21:04:05 +0800

    Shopping program

--- test.py
+++ test.py
@@ -1,7 +1,39 @@
-import time
+groceries = []
+prices = []
+total = 0
+registeredGroceries = ["apple", "banana", "grapes", "orange"]
+registeredPrices = [2, 3, 6, 4]
 
-timer = int(input("Enter countdown: "))
-for i in range(timer):
-    print(timer - i)
-    time.sleep(1)
-print("Times up")
+running = True
+
+while running: 
+    choice = input("Buy  |  Pay\n")
+    if (choice == "Pay"):
+        running = False
+    elif (choice == "Buy"):
+        grocery = input("Enter grocery: ")
+        y = 0
+        found = False
+        for x in registeredGroceries:
+            found = False
+            if (grocery.lower() == x):
+                prices.append(registeredPrices[registeredGroceries.index(grocery.lower())])
+                found = True
+                break
+            else:
+                continue
+                
+        if (not found):
+            y = float(input("Item not recognised, Enter price: "))
+            prices.append(y)
+
+        groceries.append(grocery)
+        print(f"{grocery} has been added to cart")
+        
+
+for g in groceries:
+    p = prices[groceries.index(g)]
+    print(f"{g}: ${p:.2f}")
+    total += p
+
+print(f"Total Price: ${total:.2f}")
