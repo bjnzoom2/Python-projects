1674eaf53a70e72b2819a711320ad2ab4c2d203a
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-19 12:49:12 +0800

    2d Arrays

--- test.py
+++ test.py
@@ -1,39 +1,8 @@
-groceries = []
-prices = []
-total = 0
-registeredGroceries = ["apple", "banana", "grapes", "orange"]
-registeredPrices = [2, 3, 6, 4]
+array = [1, 4, 2, 8, "G"]
+array2 = [6, False, "W", 4.1]
 
-running = True
-
-while running: 
-    choice = input("Buy  |  Pay\n")
-    if (choice == "Pay"):
-        running = False
-    elif (choice == "Buy"):
-        grocery = input("Enter grocery: ")
-        y = 0
-        found = False
-        for x in registeredGroceries:
-            found = False
-            if (grocery.lower() == x):
-                prices.append(registeredPrices[registeredGroceries.index(grocery.lower())])
-                found = True
-                break
-            else:
-                continue
-                
-        if (not found):
-            y = float(input("Item not recognised, Enter price: "))
-            prices.append(y)
-
-        groceries.append(grocery)
-        print(f"{grocery} has been added to cart")
-        
-
-for g in groceries:
-    p = prices[groceries.index(g)]
-    print(f"{g}: ${p:.2f}")
-    total += p
-
-print(f"Total Price: ${total:.2f}")
+array2d = [array, array2]
+print(array2d)
+for i in range(len(array2d)):
+    for j in range(len(array2d[i])):
+        print(array2d[i][j])
