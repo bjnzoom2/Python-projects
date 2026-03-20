9a6ba17c51b3b05096f37b560c857d77bb04a554
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-20 20:14:08 +0800

    Quiz

--- test.py
+++ test.py
@@ -1,8 +1,20 @@
-array = [1, 4, 2, 8, "G"]
-array2 = [6, False, "W", 4.1]
+questions = ("What is the speed of light", "What is this coded in", "TetoTetoTetoTeto")
+options = (("299792458 m/s", "300000000 m/s", "1 km/h", "0401"), ("Java", "C++", "Python", "Lua"), ("Teto", "Teto", "Teto", "Teto"))
+answers = ("299792458 m/s", "Python", "Teto")
 
-array2d = [array, array2]
-print(array2d)
-for i in range(len(array2d)):
-    for j in range(len(array2d[i])):
-        print(array2d[i][j])
+running = True
+while running:
+    score = 0
+    for i in range(len(questions)):
+        print(questions[i])
+        for j in range(len(options[i])):
+            print(f"{chr(j + 65)}: {options[i][j]}")
+        answer = input("")
+        if (options[i][ord(answer) - 65] == answers[i]):
+            score += 1
+            print("Correct")
+        else:
+            print("Incorrect")
+    print(f"Final Score: {score}/{len(questions)}")
+    if (input("Retry? (y/n)\n") == "n"):
+        running = False
