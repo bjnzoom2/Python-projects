47ccd0514c3422039287d5e5fe16b517f94a41a6
bjnzoom2 <bjnzoom2@gmail.com>
2026-03-20 20:30:39 +0800

    Dictionaries

--- test.py
+++ test.py
@@ -1,20 +1,15 @@
-questions = ("What is the speed of light", "What is this coded in", "TetoTetoTetoTeto")
-options = (("299792458 m/s", "300000000 m/s", "1 km/h", "0401"), ("Java", "C++", "Python", "Lua"), ("Teto", "Teto", "Teto", "Teto"))
-answers = ("299792458 m/s", "Python", "Teto")
+dictionary = {
+    "a" : 1234,
+    "banana" : "akjd",
+    "zepto" : 0.000000000000000000001
+}
 
-running = True
-while running:
-    score = 0
-    for i in range(len(questions)):
-        print(questions[i])
-        for j in range(len(options[i])):
-            print(f"{chr(j + 65)}: {options[i][j]}")
-        answer = input("")
-        if (options[i][ord(answer) - 65] == answers[i]):
-            score += 1
-            print("Correct")
-        else:
-            print("Incorrect")
-    print(f"Final Score: {score}/{len(questions)}")
-    if (input("Retry? (y/n)\n") == "n"):
-        running = False
+print(dictionary["zepto"])
+dictionary.update({"Germany" : "Berlin"})
+dictionary.pop("a")
+
+print(dictionary)
+
+print("\n")
+for i, j in dictionary.items():
+    print(i, j)
