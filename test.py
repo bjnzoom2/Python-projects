279cdb5f1ee248e29179984d52fbaaf320fa4c62
bjnzoom2 <bjnzoom2@gmail.com>
2026-04-12 18:23:01 +0800

    Funcs and python oop

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
-import random
-guessed = False
+class Car: 
+    speed = 0
+    
+    def __init__(self):
+        print("Hi")
+        
+    def accelerate(self, accel):
+        self.speed += accel
+        
+car1 = Car()
 
-while not guessed:
-    num = random.randint(0, 100)
-    if (input("Enter number: ") == str(num)):
-        print("Guessed correctly")
-        guessed = True
-    else:
-        print(f"Try again\nNumber was: {num}")
+for i in range(10):
+    car1.accelerate(i)
+    print("Speed:", car1.speed)
